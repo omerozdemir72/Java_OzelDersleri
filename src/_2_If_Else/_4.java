9b8b1cee4ab5f9190a6b69f1d41cafa4c9223ca0
omerozdemir72 <omerozdemir7274@gmail.com>
2020-12-23 20:26:21 +0100

    ELSE İF soru

---
+++ src/_2_If_Else/_4.java
@@ -0,0 +1,28 @@
+package _2_If_Else;
+
+public class _4 {
+    public static void main(String[] args) {
+
+/*
+
+Sistem size            "Bir String giriniz."   cümlesini versin.
+
+hepsi if in içinde olacak !
+
+Eğer Stringde    a karakteri varsa,   a karakterinden sonraki kısım ile yeni bir string kaydedilsin ve tüm harfleri büyük olsun. (a karakteri dahil)
+Daha sonra     ilk stringi yazdırınız, altına da a karakterinden sonraki kısma sahip olan Stringi yazdırınız
+
+Eğer a karakteri yoksa "Girdiğiniz Stringde a karakteri yoktur..     mesajını versin.
+
+örn:    String giriniz:
+    If else çok kolay gibi
+
+ Yeni String= AY GİBİ
+ Eski String = çok kolay gibi
+
+ */
+
+
+
+    }
+}
