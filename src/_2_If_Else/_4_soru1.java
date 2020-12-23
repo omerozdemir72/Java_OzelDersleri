3d32d1539e80fc617c35dcf771ef7fc1bfc4d0a0
omerozdemir72 <omerozdemir7274@gmail.com>
2020-12-23 20:27:25 +0100

    ELSE İF soru

--- src/_2_If_Else/_4_soru1.java
+++ src/_2_If_Else/_4_soru1.java
@@ -1,14 +1,12 @@
 package _2_If_Else;
 
-public class _4 {
+public class _4_soru1 {
     public static void main(String[] args) {
 
 /*
 
 Sistem size            "Bir String giriniz."   cümlesini versin.
-
 hepsi if in içinde olacak !
-
 Eğer Stringde    a karakteri varsa,   a karakterinden sonraki kısım ile yeni bir string kaydedilsin ve tüm harfleri büyük olsun. (a karakteri dahil)
 Daha sonra     ilk stringi yazdırınız, altına da a karakterinden sonraki kısma sahip olan Stringi yazdırınız
 
