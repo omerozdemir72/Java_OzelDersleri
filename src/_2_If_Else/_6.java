09254330ba78c3f0f8f00fc0a40536b3c327678e
omerozdemir72 <omerozdemir7274@gmail.com>
2020-12-23 21:14:18 +0100

    else if VE - VEYA -2

---
+++ src/_2_If_Else/_6.java
@@ -0,0 +1,24 @@
+package _2_If_Else;
+
+import sun.applet.Main;
+
+public class _6 {
+
+    public static void main(String[] args) {
+
+
+        int value1 = 1;
+        int value2 = 2;
+        int value3= 3;
+        int value4 = 4;
+        int value5 = 5;
+
+        //ikisinin de saglaması gerekiyor.
+        if((value1 == 1) && (value2 == 2 && value3==3 && value4==4 && value5==9))
+            System.out.println("value1 is 1 AND value2 is 2");
+        //ikisinden biri koşulu sağlıyorsa kod yazdırılır.
+        if((value1 == 1) || (value2 == 1 || value3==2||value4==1 || value5==9 ))
+            System.out.println("value1 is 1 OR value2 is 1");
+    }
+    }
+
