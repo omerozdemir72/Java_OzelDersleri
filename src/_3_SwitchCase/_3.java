555130e18fcc26486f21fbf9dfda58059626d88b
omerozdemir72 <omerozdemir7274@gmail.com>
2020-12-25 22:29:32 +0100

    switch case

--- src/_3_SwitchCase/_3.java
+++ src/_3_SwitchCase/_3.java
@@ -1,4 +1,47 @@
 package _3_SwitchCase;
 
+import java.util.Scanner;
+
 public class _3 {
+    public static void main(String[] args) {
+/*
+4 e kadar sayı giriniz.
+Bu 4 sayıdan farklı bir  sayı girilirse "geçersiz sayı" uyarısını versin.
+ */
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Sayı gir :");
+
+        int sayi = sc.nextInt();
+                    //5
+        switch (sayi) {//Eğer sayı,
+
+            case 1: //bir ise,
+                System.out.println("Bir");
+
+                break; //koşulu bitirir.
+
+            case 2: //2 ise
+                System.out.println("iki");
+
+                break;
+
+
+            case 3://3 ise
+                System.out.println("üç");
+
+                break;
+            case 4:
+                System.out.println("dört");
+
+
+            default://else gibi düşünün. Yukarıdakilerden hiçbiri değilse, yani 1,2,3,4 değil ise
+
+                System.out.println("Geçersiz sayı");
+        }
+
+
+
+
+    }
 }
