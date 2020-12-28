4bcaff1eb8f1b2dcdbaf281b63f99058553d8417
omerozdemir72 <omerozdemir7274@gmail.com>
2020-12-28 21:11:04 +0100

    While Giriş

---
+++ src/_5_While/_1.java
@@ -0,0 +1,38 @@
+package _5_While;
+
+public class _1 {
+    public static void main(String[] args) {
+//
+//        for (int i = 0; i <= 5; i++) {
+//            system.out.print(i);
+//        }
+
+        int sayac = 0; //sayac kaçtan başlayacak?
+        while (sayac<=5){   //0'dan kaça kadar devam edecek ? Döngü kaç tur olacak?
+            System.out.print(sayac);
+
+            sayac++; //Sayaç 1er 1 er artacak >>>   0     1     2    3    4     5
+        }
+        System.out.println();
+
+        System.out.println(" -------------------********------------------");
+        System.out.println("FOR DÖNGÜSÜ TERSTEN YAZDIRMA ");
+
+        for (int i = 10; i >0 ; i--) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println();
+        System.out.println("----WHİLE DÖNGÜSÜ İLE TERSTEN YAZDIRMA -----");
+
+            int sayac2 =10;
+
+            while (sayac2>0){
+                System.out.print(sayac2 + " | ");
+
+                sayac2--;
+
+            }
+
+    }
+}
