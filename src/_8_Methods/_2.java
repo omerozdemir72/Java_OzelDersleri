62ceb5fd282cc944a38512f47e985290b0c9a8ca
omerozdemir72 <omerozdemir7274@gmail.com>
2021-01-03 20:37:33 +0100

    Methodlar

---
+++ src/_8_Methods/_2.java
@@ -0,0 +1,33 @@
+package _8_Methods;
+
+public class _2 {
+    public static void main(String[] args) {
+        /*
+void methodlar dışında bir de dönüşü olan methodlarımız vardır. Bunları değişken bazlı oluştururuz.
+
+ */
+
+        System.out.println(topla(10,20));
+        System.out.println(cikar(20,5));
+        System.out.println(bol(20,5));
+        System.out.println(carp(20,5));
+    }
+
+    public static int topla(int a, int b){
+
+   return a+b;//a ve b nin toplamını bana main methoddaki degerlere göre döndür.
+
+    }
+
+    public static int cikar(int a, int b){
+        return a - b;
+    }
+
+    public static int bol(int a, int b){
+        return a /b;
+    }
+    public static int carp(int a, int b){
+        return a*b;
+    }
+
+}
