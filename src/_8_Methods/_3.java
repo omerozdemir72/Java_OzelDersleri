2973a081f4f2012a0dd35168f8a0736097313fad
omerozdemir72 <omerozdemir7274@gmail.com>
2021-01-03 20:47:56 +0100

    Methodlar

---
+++ src/_8_Methods/_3.java
@@ -0,0 +1,29 @@
+package _8_Methods;
+
+public class _3 {
+/*
+Bir "static void" methodu oluşturun.
+
+methodun ismi EkranaYaz   olsun.
+
+Bir String a parametresi alsın.
+method içinde bu string "yazdırılsın".
+
+Main methodda cagırdıgınızda içerisine" Merhaba Dünya" yazısını yazdıgınızda bu yazıyı konsolda görelim.
+ */
+
+
+public static void main(String[] args) {
+EkranaYaz("Merhaba");
+}
+
+static void EkranaYaz(String a){
+
+    System.out.println(a);
+
+}
+
+
+
+}
+
---
+++ src/_8_Methods/_4.java
@@ -0,0 +1,17 @@
+package _8_Methods;
+
+public class _4 {
+    public static void main(String[] args) {
+
+        /*
+        public static String methodu oluşturun.
+        ismi EkranaYaz olsun.
+
+       Parametre olarak String a alsın.
+
+       ve String a 'yı return etsin.
+
+       Daha sonra bu methodu main methoda cagırın. İçerisine bir string değer girin ve yazdırın.
+         */
+    }
+}
