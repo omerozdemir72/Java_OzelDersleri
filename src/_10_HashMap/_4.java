d6c97514a5521396233bf19db2867db5faec707b
omerozdemir72 <omerozdemir7274@gmail.com>
2021-01-06 20:17:41 +0100

    HashMap

---
+++ src/_10_HashMap/_4.java
@@ -0,0 +1,15 @@
+package _10_HashMap;
+
+public class _4 {
+    /*
+    Ömer
+    Hüseyin
+    Ahmet
+    Meral
+    Tufan
+
+    bir hashmap oluşturun. Key kısmına 1,2,3,4,5     value kısmına ise yukarıdaki isimleri sırayla girin.
+
+    Daha sonra bunları konsola yazdırın.
+     */
+}
