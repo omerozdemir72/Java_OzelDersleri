e3cf287bbb9c4f62623cfb15044fcfd714328644
omerozdemir72 <omerozdemir7274@gmail.com>
2021-01-06 20:55:00 +0100

    Hashmap Sorular

---
+++ src/_10_HashMap/_10.java
@@ -0,0 +1,23 @@
+package _10_HashMap;
+
+public class _10 {
+    public static void main(String[] args) {
+
+        /*
+
+      Bir hashmap oluşturun. Key -Integet
+      Value - Integer
+
+     1,   10
+     2,   20
+     3,   30
+     4,   40
+
+
+     Eğer key lerin içerisinde 3 varsa
+     konsola 3'ün DEĞERİNİ yazdırın.
+
+         */
+
+    }
+}
