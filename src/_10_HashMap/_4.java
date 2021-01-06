aba32bd4ac5333f098459fc06909042ae8a3f205
omerozdemir72 <omerozdemir7274@gmail.com>
2021-01-06 20:30:52 +0100

    HashMap

--- src/_10_HashMap/_4.java
+++ src/_10_HashMap/_4.java
@@ -1,5 +1,7 @@
 package _10_HashMap;
 
+import java.util.HashMap;
+
 public class _4 {
     /*
     Ömer
@@ -12,4 +14,25 @@ public class _4 {
 
     Daha sonra bunları konsola yazdırın.
      */
+
+    public static void main(String[] args) {
+        HashMap<Integer,String> map = new HashMap<>();
+
+        map.put(1,"Omer");
+        map.put(2,"Huseyin");
+        map.put(3,"Ahmet");
+        map.put(4,"Meral");
+        map.put(5,"Tufan");
+
+        System.out.println(map);
+
+        System.out.println(map.size());
+
+        /*
+        map2 oluşturun. ve map teki elemanları map2 ye kopyalayın. ve map2 yi yazdırın.
+         */
+        HashMap<Integer,String> map2 = new HashMap<>();
+        map2.putAll(map);
+        System.out.println("map2:  "  +map2);
+    }
 }
