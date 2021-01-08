e36592f6d568f5a48437e23d760004f86ffa894f
omerozdemir72 <omerozdemir7274@gmail.com>
2021-01-08 20:33:33 +0100

    Encapsulation giris

---
+++ src/_11_Encapsulation/Soru1/calisan.java
@@ -0,0 +1,13 @@
+package _11_Encapsulation.Soru1;
+
+public class calisan {
+    /*
+    calisan classında  String isim
+    int calisanSayisi
+    int yas
+    oluşturun.
+
+    bunların getter ve setter larını oluşturun.
+
+     */
+}
---
+++ src/_11_Encapsulation/Soru1/mainClass.java
@@ -0,0 +1,13 @@
+package _11_Encapsulation.Soru1;
+
+public class mainClass {
+    public static void main(String[] args) {
+
+        /*
+        calisan classı için bir referans oluşturun.
+        Daha sonra calisan classındaki sstring ve int lere değer verin.
+
+        ve bunları konsola yazdırın.
+         */
+    }
+}
