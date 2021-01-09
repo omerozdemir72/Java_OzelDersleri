12b88112ae5dd74a2116e8ba0336f23c17d3aeb8
omerozdemir72 <omerozdemir7274@gmail.com>
2021-01-09 20:21:15 +0100

    Try Catch

---
+++ src/_12_TryCatch/_1_.java
@@ -0,0 +1,25 @@
+package _12_TryCatch;
+
+public class _1_ {
+    public static void main(String[] args) {
+/*
+Normalde hata alıp devam edemeyecek bir kodu, try kısmının içerisine yazdıgınızda
+eğer hata varsa hatayı yakalar, hangi hata oldugunu söyler (hangi exception)  ve tüm kodlar
+calısmaya devam eder.
+ */
+        try { //Kodları buraya yazıyoruz. Hata ihtimali olan veya olmayan
+            int [] a = {1,2,3,4,5};
+            a[7]=6;
+        }catch (ArithmeticException e){ //Sayılar ile ilgili  5/0;
+            System.out.println("Hata 1:  " + e.getMessage());
+
+        }catch (ArrayIndexOutOfBoundsException e){//indeks aşımı
+       //    e.printStackTrace();//gerçek hataymış gibi konsolda bilgi veriyor
+       //       System.out.println( e); //java.lang.ArrayIndexOutOfBoundsException: 7
+            System.out.println(e.getMessage()); //7
+            e.printStackTrace(); //kırmızı renk ile gerçek bir hata gibi konsolda bilgi veriyor
+        }
+
+        System.out.println("aaaaaa");
+    }
+}
